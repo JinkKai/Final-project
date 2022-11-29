3f40ef0f28f9a3b7a711c15ed1b056e58b62ccb3
JinkKai <jinkai.chen@my.marianas.edu>
2022-11-29 18:01:38 +1000

    third draft

--- javascript/style.js
+++ javascript/style.js
@@ -6,3 +6,18 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+hamburger.addEventListener('click', ()=>{
+   //Links
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle("fade");
+    });
+
+    //Animation
+    hamburger.classList.toggle("toggle");
+});
